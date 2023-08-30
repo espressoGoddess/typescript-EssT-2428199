82ca4b8e50170dd0e06b7913917dce5cbd2782e0
Amber Shipley <ashipley2@me.com>
2023-08-29 17:08:50 -0700

    Type first file and install ts-node

--- exercise_files/Ch02/02_Challenge/app.ts
+++ exercise_files/Ch02/02_Challenge/app.ts
@@ -1,27 +1,47 @@
-const todoItems = [
-    { id: 1, title: "Learn HTML", status: "done", completedOn: new Date("2021-09-11") },
-    { id: 2, title: "Learn TypeScript", status: "in-progress" },
-    { id: 3, title: "Write the best app in the world", status: "todo" },
-]
-
-function addTodoItem(todo) {
-    const id = getNextId(todoItems)
-
-    const newTodo = {
-        id,
-        title: todo,
-        status: "todo",
-    }
-
-    todoItems.push(newTodo)
-
-    return newTodo
+enum ToDoStatus {
+  Done = "done",
+  InProgress = "in-progress",
+  ToDo = "to-do",
 }
 
-function getNextId(items) {
-    return items.reduce((max, x) => x.id > max ? x.id : max, 0) + 1
+interface ToDoItem {
+  id: number;
+  title: string;
+  status: string;
+  completedOn?: Date;
 }
 
-const newTodo = addTodoItem("Buy lots of stuff with all the money we make from the app")
+const todoItems: Array<ToDoItem> = [
+  {
+    id: 1,
+    title: "Learn HTML",
+    status: ToDoStatus.Done,
+    completedOn: new Date("2021-09-11"),
+  },
+  { id: 2, title: "Learn TypeScript", status: ToDoStatus.InProgress },
+  { id: 3, title: "Write the best app in the world", status: ToDoStatus.ToDo },
+];
 
-console.log(JSON.stringify(newTodo))
+function addTodoItem(todo: string): ToDoItem {
+  const id = getNextId(todoItems);
+
+  const newTodo = {
+    id,
+    title: todo,
+    status: "todo",
+  };
+
+  todoItems.push(newTodo);
+
+  return newTodo;
+}
+
+function getNextId(items: Array<ToDoItem>): number {
+  return items.reduce((max, x) => (x.id > max ? x.id : max), 0) + 1;
+}
+
+const newTodo = addTodoItem(
+  "Buy lots of stuff with all the money we make from the app"
+);
+
+console.log(JSON.stringify(newTodo));
